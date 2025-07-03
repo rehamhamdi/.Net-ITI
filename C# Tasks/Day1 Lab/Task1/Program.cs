cc2450ff4ee086d8b772d43e91a2082eed1102e3
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2025-07-03 17:35:41 +0300

    Add files via upload

---
+++ C# Tasks/Day1 Lab/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Tasks_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers will you enter? ");
+            int enter_number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int count_even = 0;
+            int count_odd = 0;
+            int x;
+            int max = 0, min = 100000;
+            for (int i = 0; i < enter_number; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                x = int.Parse(Console.ReadLine());
+                sum += x;
+                if (x % 2 == 0) count_even++;
+                else count_odd++;
+                if (x > max) max = x;
+                if (x < min) min = x;
+            }
+
+            if (count_even > count_odd)
+                Console.WriteLine("Most of your numbers are even.");
+            else Console.WriteLine("Most of your numbers are odd.");
+            Console.WriteLine($"Sum: {sum}");
+            int average = sum / enter_number;
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+
+        }
+    }
+}
---
+++ C# Tasks/Day1 Lab/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task2_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intial_balance = 1000;
+            int option;
+            Console.WriteLine("Welcome to the ATM!");
+            do
+            {
+                Console.WriteLine("\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\n");
+                Console.Write("Choose an option: ");
+                option = int.Parse(Console.ReadLine());
+
+                if (option == 1)
+                {
+                    Console.WriteLine($"your Balance is: {intial_balance}");
+                }
+                else if (option == 2)
+                {
+                    Console.Write("Enter amount to deposit: ");
+                    int deposit = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Balance is now: {intial_balance + deposit}");
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("enter amount to withdraw");
+                    int withdraw = int.Parse(Console.ReadLine());
+                    if (withdraw > intial_balance)
+                    {
+                        Console.WriteLine("you can not withdraw amount more than your balance");
+                    }
+                    else { Console.WriteLine("the withdraw opration is done succefully"); }
+                }
+            } while (option != 4);
+        }
+    }
+}
