28a441e5d0e83e838d55c1b2d89b14c9b695d93e
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2025-07-09 18:06:36 +0300

    Add files via upload

---
+++ C# Tasks/Day2 Lab/String Manipulation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace String_Manipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string sentence = Console.ReadLine().ToLower();
+
+            int count = 0;
+
+            foreach (char c in sentence)
+            {
+                if ("aeiou".Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Number of vowels: " + count);
+        }
+    }
+}
