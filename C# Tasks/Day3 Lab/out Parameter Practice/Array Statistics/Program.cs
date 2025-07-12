61d3365c3191f68e33c234d8d16508bc0891cbe3
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2025-07-12 17:11:18 +0300

    Add files via upload

---
+++ C# Tasks/Day3 Lab/out Parameter Practice/Array Statistics/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+
+namespace Array_Statistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("Enter Scores: ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            CalculateArrayStats(array , out int max , out int min , out double avr );
+            Console.WriteLine($"Max = {max}, Min = {min}, Average = {avr}");
+        }
+        static void CalculateArrayStats(int[] scores, out int max, out int min, out double average)
+        {
+            
+            //MAx
+            max = int.MinValue;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] > max)
+                {
+                    max= scores[i];
+                }
+            }
+
+            //Min
+            min = int.MaxValue;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] < min)
+                {
+                    min = scores[i];
+                }
+            }
+
+            //Average
+            int sum = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                sum += scores[i];
+            }
+            average = sum / scores.Length;
+        }
+    }
+}
---
+++ C# Tasks/Day3 Lab/out Parameter Practice/Division Helper/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Division_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Two Integers: ");
+            int dividend = int.Parse(Console.ReadLine());
+            int divisor = int.Parse(Console.ReadLine());
+            DivideAndRemainder(dividend,divisor, out int q , out int r);
+            Console.WriteLine($"The quotient is= {q}");
+            Console.WriteLine($"The remainder is= {r}");
+        }
+
+        static void DivideAndRemainder(int dividend, int divisor, out int quotient, out int remainder)
+        {
+            quotient =dividend / divisor;
+            remainder = dividend % divisor;
+
+        }
+    }
+}
